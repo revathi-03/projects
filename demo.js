c098c84e2256a19625dcc209cb2432cc78324b81
unknown <DT035@DT035.tmfsmart.com>
2023-12-01 12:23:59 +0530

    update

--- demo.js
+++ demo.js
@@ -1,5 +1,12 @@
-<script>
-    var node= document.creatElement("p");
-    var content=createTextNode("this is file");
+
+    //html elements in javascript
+    var node= document.createElement("p");
+    var content=createTextNode("this is file para");
     node.appenChild(content);
-</script>
+    var root=document.getElementById("root");
+    root.appenChild(root);
+    //html elements in react
+    const elment=react.createElement(h1,{},);
+   // const node=react.createTextNode("this is heading from react");
+
+    
