34a9e5fdc82c50c68d3fdd163be5430d60cee18f
revathi-03 <150006128+revathi-03@users.noreply.github.com>
2024-02-23 15:48:47 +0530

    Add files via upload

---
+++ java/dto/UserInfo.java
@@ -0,0 +1,77 @@
+package com.revathi.project.dto;
+
+public class UserInfo {
+	private int userid;
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getFname() {
+		return Fname;
+	}
+
+	public void setFname(String fname) {
+		Fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getContact() {
+		return contanct;
+	}
+
+	public void setContact(String contanct) {
+		this.contanct = contanct;
+	}
+
+	private String Fname;
+	private String lname;
+	private String email;
+	private String password;
+	private String address;
+	private String gender;
+	private String contanct;
+}
